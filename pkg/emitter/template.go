c78fcbf11dcda8c794ad5e3395b330567d856180
tdakkota <tanc13@yandex.ru>
2020-09-15 13:22:14 +0300

    feat: add default template

--- pkg/emitter/template.go
+++ pkg/emitter/template.go
@@ -1,16 +1,53 @@
 package emitter
 
 import (
+	"fmt"
 	"text/template"
 
 	"github.com/tdakkota/vkalertmanager/pkg/hook"
 	"github.com/valyala/bytebufferpool"
 )
 
+const defaultTemplate = `
+{{ range .Alerts }}
+{{ if eq .Status "firing"}}🔥 <b>{{ .Status | toUpper }}</b> 🔥{{ else }}<b>{{ .Status | toUpper }}</b>{{ end }}
+<b>{{ .Labels.alertname }}</b>
+{{ if .Annotations.message }}
+{{ .Annotations.message }}
+{{ end }}
+{{ if .Annotations.summary }}
+{{ .Annotations.summary }}
+{{ end }}
+{{ if .Annotations.description }}
+{{ .Annotations.description }}
+{{ end }}
+<b>Duration:</b> {{ duration .StartsAt .EndsAt }}{{ if ne .Status "firing"}}
+<b>Ended:</b> {{ .EndsAt | since }}{{ end }}
+{{ end }}
+`
+
+func Default() Template {
+	t, err := Parse(defaultTemplate)
+	if err != nil {
+		panic(err)
+	}
+
+	return t
+}
+
+func ParseFiles(path ...string) (Template, error)  {
+	t, err := template.New("msg").ParseFiles(path...)
+	if err != nil {
+		return Template{}, fmt.Errorf("failed to parse template: %w", err)
+	}
+
+	return NewTemplate(t), nil
+}
+
 func Parse(s string) (Template, error) {
 	t, err := template.New("msg").Parse(s)
 	if err != nil {
-		return Template{}, err
+		return Template{}, fmt.Errorf("failed to parse template: %w", err)
 	}
 
 	return NewTemplate(t), nil
@@ -33,5 +70,8 @@ func (t Template) Produce(m hook.Message) (string, error) {
 	defer t.pool.Put(b)
 
 	err := t.template.Execute(b, m)
-	return b.String(), err
+	if err != nil {
+		return "", err
+	}
+	return b.String(), nil
 }
--- pkg/server/config.go
+++ pkg/server/config.go
@@ -1,21 +1,5 @@
 package server
 
-type Config struct {
-	VK
-	Template Template       `json:"template" yaml:"template"`
-	Listener ListenerConfig `json:"listeners" yaml:"listeners"`
-}
-
-type Template struct {
-	File string `json:"file" yaml:"file"`
-	Body string `json:"body" yaml:"body"`
-}
-
-type VK struct {
-	UserAgent string   `json:"user_agent" yaml:"user_agent"`
-	Tokens    []string `json:"tokens" yaml:"tokens"`
-}
-
 type ListenerConfig struct {
 	Bind string     `json:"bind" yaml:"bind"`
 	TLS  *TLSConfig `json:"tls" yaml:"tls"`
