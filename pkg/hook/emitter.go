59960daaea8f13cb8f9f16c4b2af4e0daefc097b
tdakkota <tanc13@yandex.ru>
2020-09-18 21:10:45 +0300

    feat: add EmitterFunc type

--- pkg/hook/emitter.go
+++ pkg/hook/emitter.go
@@ -7,3 +7,9 @@ import (
 type Emitter interface {
 	Emit(ctxt context.Context, m Message) error
 }
+
+type EmitterFunc func(ctxt context.Context, m Message) error
+
+func (e EmitterFunc) Emit(ctxt context.Context, m Message) error {
+	return e(ctxt, m)
+}
